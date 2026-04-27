c1df34c142d33ffed4210c963f6dea7158c10714
marlenecastillano14344-collab <marlenecastillano14344@gmail.com>
2026-04-27 22:17:15 +0800

    Update Index.php

--- Index.php
+++ Index.php
@@ -1,4 +1,13 @@
 <?php
+
+echo "Activity 1"
 echo "Name: Juan Dela Cruz <br>";
-print "Course: BSIT";
+print "Course: BSIT <br><br>";
+
 ?>
+
+Guide Questions:
+1. 2. What symbol is used to end a PHP statement? 
+answer: Semicolon ( ; )
+Which is faster: echo or print?
+answer: echo is slightly faster than print
