9537044906ec0696a03c7b6f8b22c04c0e407e34
marlenecastillano14344-collab <marlenecastillano14344@gmail.com>
2026-04-27 22:50:23 +0800

    Create Mini project.php

---
+++ Mini project.php
@@ -0,0 +1,9 @@
+<?php
+$num1 = 8;
+$num2 = 4;
+
+echo "Sum: " . ($num1 + $num2) . "<br>";
+echo "Difference: " . ($num1 - $num2) . "<br>";
+echo "Product: " . ($num1 * $num2) . "<br>";
+echo "Quotient: " . ($num1 / $num2);
+?>
